3cd9e6547065f328e43ff79f88008e48dd6981e2
Ethan <epatter2@ggc.edu>
2016-02-17 03:32:55 +0000

    update2

--- tests/test_semester.rb
+++ tests/test_semester.rb
@@ -7,7 +7,10 @@ class TestSemester < Minitest::Test
     assert_equal(Semester.new.next_semesters("Spring 2016"),("Summer 2016"))
     assert_equal(Semester.new.next_semesters("Summer 2016"),("Fall 2016"))
     assert_equal(Semester.new.next_semesters("Fall 2016"),("Spring 2017"))
-    
+    assert_equal(Semester.new.next_semester("Spring 2016", 4).last,("Summer 2017"))
+    assert_equal(Semester.new.next_semester("Fall 2016",20).last,("Summer 2023"))
+    assert_equal(Semester.new.next_semester("Fall 2015",0).length,1)
+    assert_equal(Semester.new.next_semester("Fall 2015",3).length,4)
   end
 
 end
